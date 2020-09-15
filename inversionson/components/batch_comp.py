make event weighting more aggressive and fix issue in grad retrieval

inversionson/components/batch_comp.py
@@ -417,8 +417,9 @@ class BatchComponent(Component):
         #     angular_changes[event] = angle
         #     event_quality[event] = 0.0
         batch_grad = np.copy(full_grad)
-
+        removal_order = 0
         while len(ctrl_group) > min_ctrl:
+	    removal_order += 1
             event_name, test_batch_grad = self._find_most_useless_event(
                 full_gradient=batch_grad,
                 events=ctrl_group,
@@ -443,7 +444,8 @@ class BatchComponent(Component):
             else:
                 batch_grad = np.copy(test_batch_grad)
                 # del angular_changes[redundant_gradient]
-                event_quality[event_name] = 1 / len(ctrl_group)
+                # event_quality[event_name] = 1 / len(ctrl_group)
+                event_quality[event_name] = removal_order / len(events)
                 ctrl_group.remove(event_name)
                 print(f"{event_name} does not continue to next iteration")
         if "it0000" not in iteration: