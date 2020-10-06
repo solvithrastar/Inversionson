c9977210b1e08f209d3b030e8c31b1366d5f8607
Dirk-Philip van Herwaarden <dirkphilip@DirkPhilipsMBP.fritz.box>
2020-10-06 17:14:10 +0200

    Changed the autoinverter structure, such that there is no deep recursion into the assign_task_function. This should make the code mpre memory and stack efficient.

--- inversionson/autoinverter.py
+++ inversionson/autoinverter.py
@@ -1648,7 +1648,7 @@ class AutoInverter(object):
             message += "It gave an error and the task.toml has not been "
             message += "updated."
             raise InversionsonError(message)
-        self.assign_task_to_function(task_2, verbose_2)
+        return task_2, verbose_2
 
     def compute_misfit(self, task: str, verbose: str):
         """
@@ -1797,7 +1797,7 @@ class AutoInverter(object):
             message += "It gave an error and the task.toml has not been "
             message += "updated."
             raise InversionsonError(message)
-        self.assign_task_to_function(task_2, verbose_2)
+        return task_2, verbose_2
 
     def compute_gradient(self, task: str, verbose: str):
         """
@@ -1946,7 +1946,7 @@ class AutoInverter(object):
             message += "It gave an error and the task.toml has not been "
             message += "updated."
             raise InversionsonError(message)
-        self.assign_task_to_function(task_2, verbose_2)
+        return task_2, verbose_2
 
     def select_control_batch(self, task, verbose):
         """
@@ -1996,7 +1996,7 @@ class AutoInverter(object):
             message += "It gave an error and the task.toml has not been "
             message += "updated."
             raise InversionsonError(message)
-        self.assign_task_to_function(task_2, verbose_2)
+        return task_2, verbose_2
 
     def finalize_iteration(self, task, verbose):
         """
@@ -2028,7 +2028,7 @@ class AutoInverter(object):
             message += "It gave an error and the task.toml has not been "
             message += "updated."
             raise InversionsonError(message)
-        self.assign_task_to_function(task_2, verbose_2)
+        return task_2, verbose_2
 
     def assign_task_to_function(self, task: str, verbose: str):
         """
@@ -2043,17 +2043,19 @@ class AutoInverter(object):
         print(f"More specifically: {verbose}")
 
         if task == "compute_misfit_and_gradient":
-            self.compute_misfit_and_gradient(task, verbose)
+            task, verbose = self.compute_misfit_and_gradient(task, verbose)
         elif task == "compute_misfit":
-            self.compute_misfit(task, verbose)
+            task, verbose = self.compute_misfit(task, verbose)
         elif task == "compute_gradient":
-            self.compute_gradient(task, verbose)
+            task, verbose = self.compute_gradient(task, verbose)
         elif task == "select_control_batch":
-            self.select_control_batch(task, verbose)
+            task, verbose = self.select_control_batch(task, verbose)
         elif task == "finalize_iteration":
-            self.finalize_iteration(task, verbose)
+            task, verbose = self.finalize_iteration(task, verbose)
         else:
             raise InversionsonError(f"Don't know task: {task}")
+
+        return task, verbose
 
     def run_inversion(self):
         """
@@ -2070,7 +2072,8 @@ class AutoInverter(object):
         task, verbose = self.comm.salvus_opt.read_salvus_opt_task()
         self.task = task
 
-        self.assign_task_to_function(task, verbose)
+        while True:
+            task, verbose = self.assign_task_to_function(task, verbose)
 
 
 def read_information_toml(info_toml_path: str):
