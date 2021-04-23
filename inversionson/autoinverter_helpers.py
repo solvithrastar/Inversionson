255d3d92fb3fa96e68b603b820cc6a23edec3969
solvithrastar <solvithrastar@gmail.com>
2021-04-23 15:27:24 +0000

    Helpers implemented for autoinverter, needs testing

--- inversionson/autoinverter_helpers.py
+++ inversionson/autoinverter_helpers.py
@@ -3,10 +3,23 @@ from colorama import init
 from colorama import Fore, Style
 import time
 import os
+import inspect
 import warnings
 import glob
 import shutil
+import toml
 from inversionson import InversionsonError, InversionsonWarning
+from salvus.flow.api import get_site
+
+CUT_SOURCE_SCRIPT_PATH = os.path.join(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(inspect.getfile(inspect.currentframe()))
+        )
+    ),
+    "remote_scripts",
+    "cut_and_clip.py",
+)
 
 init()
 
@@ -996,6 +1009,62 @@ class AdjointHelper(object):
             print(f"Gradient for event {event} has been retrieved.")
         else:
             self.comm.smoothing.run_remote_smoother(event)
+
+    def __cut_and_clip_gradient(self, event):
+        """
+        Cut sources and receivers from gradient before smoothing.
+        We also clip the gradient to some percentile
+        This can all be configured in information toml.
+
+        :param event: name of the event
+        """
+        job = self.comm.salvus_flow.get_job(event, "adjoint")
+        output_files = job.get_output_files()
+        gradient_path = output_files[0][
+            ("adjoint", "gradient", "output_filename")
+        ]
+        # Connect to daint
+        daint = get_site(self.comm.project.site_name)
+        username = daint.config["ssh_settings"]["username"]
+
+        remote_inversionson_dir = os.path.join(
+            "/scratch/snx3000", username, "smoothing_info"
+        )
+
+        if not daint.remote_exists(remote_inversionson_dir):
+            daint.remote_mkdir(remote_inversionson_dir)
+
+        # copy processing script to daint
+        remote_script = os.path.join(
+            remote_inversionson_dir, "cut_and_clip.py"
+        )
+        if not daint.remote_exists(remote_script):
+            daint.remote_put(CUT_SOURCE_SCRIPT_PATH, remote_script)
+
+        if self.comm.project.cut_receiver_radius > 0.0:
+            raise InversionsonError(
+                "Remote receiver cutting not implemented yet."
+            )
+
+        info = {}
+        info["filename"] = str(gradient_path)
+        info["cutout_radius_in_km"] = self.comm.project.cut_source_radius
+        info["source_location"] = self.comm.lasif.get_source(event_name=event)
+
+        info["clipping_percentile"] = self.comm.project.clip_gradient
+        info["parameters"] = self.comm.project.inversion_params
+
+        toml_filename = f"{event}_gradient_process.toml"
+        with open(toml_filename, "w") as fh:
+            toml.dump(info, fh)
+
+        # put toml on daint and remove local toml
+        remote_toml = os.path.join(remote_inversionson_dir, toml_filename)
+        daint.remote_put(toml_filename, remote_toml)
+        os.remove(toml_filename)
+
+        # Call script
+        print(daint.run_ssh_command(f"python {remote_script} {remote_toml}"))
 
 
 class SmoothingHelper(object):
