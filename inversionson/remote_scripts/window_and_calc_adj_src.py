fix bug station weighting

inversionson/remote_scripts/window_and_calc_adj_src.py
@@ -55,8 +55,8 @@ def get_station_weights(list_of_stations, processed_data):
     # Make reduced list:
     stations = {}
     for station in list_of_stations:
-        stations["CI.FUR"]["latitude"] = coordinates[station]["latitude"]
-        stations["CI.FUR"]["longitude"] = coordinates[station]["longitude"]
+        stations[station]["latitude"] = coordinates[station]["latitude"]
+        stations[station]["longitude"] = coordinates[station]["longitude"]
 
     locations = np.zeros((2, len(stations)), dtype=np.float64)
 