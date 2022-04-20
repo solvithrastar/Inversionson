7808d9508c0952fb8e74723de0a47d4797d7d845
Dirk-Philip <dirkphilip.vanherwaarden@erdw.ethz.ch>
2022-04-20 15:22:25 +0200

    make code prettier

--- inversionson/optimizers/regularization_helper.py
+++ inversionson/optimizers/regularization_helper.py
@@ -18,7 +18,7 @@ class RegularizationHelper(object):
         Each tasks is a dict that has a reference model, a model that contains the fields
         that require smoothing, the smoothing lengths, the parameters that require
         smoothing and the output location to which the smoothed parameters
-        should be retrieved
+        should be retrieved.
 
         tasks = {task_name: {"reference_model": str, "model_to_smooth": str,
                 "smoothing_lengths": list, "smoothing_parameters": list,
@@ -34,50 +34,39 @@ class RegularizationHelper(object):
         )
         if not os.path.exists(self.regularization_folder):
             os.mkdir(self.regularization_folder)
-        self.write_tasks(tasks)
-        self.tasks = toml.load(self.get_iteration_toml_filename())
+        self._write_tasks(tasks)
+        self.tasks = toml.load(self._get_iteration_toml_filename())
 
-    def get_iteration_toml_filename(self):
+    def _get_iteration_toml_filename(self):
         return self.regularization_folder / (self.iteration_name +
                                              "_regularization.toml")
 
-    def write_tasks(self, tasks):
+    def _write_tasks(self, tasks):
         """
         This function writes the tasks to file or updates the task file.
-
-        :param tasks = Dictionary of dictionaries with the relevant info, where
-        for the smoothing jobs. Each key in the outer dictionary represents a task.
-        :type tasks: Dict
         """
         # Write initial toml if there is no task toml yet
-        if not os.path.exists(self.get_iteration_toml_filename()):
+        base_dict = dict(job_name="", submitted=False, retrieved=False,
+                         reposts=0)
+        if not os.path.exists(self._get_iteration_toml_filename()):
             for task_dict in tasks.values():
-                task_dict["job_name"] = ""
-                task_dict["submitted"] = False
-                task_dict["retrieved"] = False
-                task_dict["reposts"] = 0
-            with open(self.get_iteration_toml_filename(), "w") as fh:
+                task_dict.update(base_dict)
+            with open(self._get_iteration_toml_filename(), "w") as fh:
                 toml.dump(tasks, fh)
 
         else:  # We add the tasks to the existing tasks if needed
-            existing_tasks = toml.load(self.get_iteration_toml_filename())
+            existing_tasks = toml.load(self._get_iteration_toml_filename())
             for task_name, task in tasks.items():
                 # Add the empty task if it does not exist
                 if task_name not in existing_tasks.keys():
                     existing_tasks[task_name] = tasks[task_name]
-                    existing_tasks[task_name]["job_name"] = ""
-                    existing_tasks[task_name]["submitted"] = False
-                    existing_tasks[task_name]["retrieved"] = False
-                    existing_tasks[task_name]["reposts"] = 0
+                    existing_tasks[task_name].update(base_dict)
                 else:  # Update existing tasks with passed tasks
                     existing_tasks[task_name] = tasks[task_name]
             with open(self.get_iteration_toml_filename(), "w") as fh:
                 toml.dump(existing_tasks, fh)
 
     def dispatch_smoothing_tasks(self):
-        """
-        Dispatches tasks that are not yet dispatched.
-        """
         for task_name, task_dict in self.tasks.items():
             if not task_dict["submitted"] and task_dict["reposts"] < self.max_reposts:
                 sims = self.comm.smoother.get_sims_for_smoothing_task(
@@ -94,9 +83,9 @@ class RegularizationHelper(object):
                 )
                 self.tasks[task_name]["submitted"] = True
                 self.tasks[task_name]["job_name"] = job.job_array_name
-                self.write_tasks(self.tasks)
+                self._write_tasks(self.tasks)
 
-    def update_tasks_and_retrieve(self):
+    def update_task_status_and_retrieve(self):
         for task_dict in self.tasks.values():
             job = sapi.get_job_array(job_array_name=task_dict["job_name"],
                                      site_name=self.site_name)
@@ -108,7 +97,7 @@ class RegularizationHelper(object):
                 elif s.name in ["unknown", "failed"]:
                     task_dict["reposts"] += 1
                     task_dict["submitted"] = False
-                    self.write_tasks(self.tasks)
+                    self._write_tasks(self.tasks)
             if finished:
                 smooth_gradient = get_smooth_model(
                     job=job,
@@ -116,7 +105,7 @@ class RegularizationHelper(object):
                 )
                 smooth_gradient.write_h5(task_dict["output_location"])
                 task_dict["retrieved"] = True
-                self.write_tasks(self.tasks)
+                self._write_tasks(self.tasks)
 
     def all_retrieved(self):
         for task_dict in self.tasks.values():
@@ -125,14 +114,9 @@ class RegularizationHelper(object):
         return True
 
     def monitor_tasks(self):
-        """
-        This functions monitors the tasks, resubmits them if they fail
-        or retrieves them
-        """
-        # Dispatch tasks that are not dispatched yet first.
         self.dispatch_smoothing_tasks()
         while not self.all_retrieved():
-            print("Attempting to retrieve smoothing jobs.")
-            self.update_tasks_and_retrieve()
+            print("Monitoring smoothing jobs...")
+            self.update_task_status_and_retrieve()
             self.dispatch_smoothing_tasks()
             sleep_or_process(self.comm)
