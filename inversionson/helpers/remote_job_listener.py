2f213cefc9829da29fb4e590e967366e0de00f72
Dirk-Philip <dirkphilip.vanherwaarden@erdw.ethz.ch>
2022-05-06 15:00:28 +0200

    Remove progress bar after checking jobs

--- inversionson/helpers/remote_job_listener.py
+++ inversionson/helpers/remote_job_listener.py
@@ -233,7 +233,8 @@ class RemoteJobListener(object):
             f"Checking Jobs for {self.job_type}:", line_above=True, emoji_alias=":ear:"
         )
         for event in tqdm(
-            events_left, desc=emoji.emojize(":ear: | ", use_aliases=True), postfix="\r"
+            events_left, desc=emoji.emojize(":ear: | ", use_aliases=True),
+                leave=False
         ):
             if job_dict[event]["retrieved"]:
                 self.events_already_retrieved.append(event)
